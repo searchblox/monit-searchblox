a79a34733f4550d760d3c9a3356aec4a9b0e8b84
Erik Geiger <Erik@geiger-online.ch>
2014-11-14 22:15:43 +0100

    enhanced script to also run on RedHat, debian, and ubuntu (besides CentOS)

--- searchblox_monit.sh
+++ searchblox_monit.sh
@@ -17,6 +17,11 @@
 me=$(basename $0)
 log_out="STDOUT"
 log_out_err="STDERR"
+#valid for ubuntu and debian
+confdir_debian="/etc/monit/conf.d/"
+conffile_debian="/etc/monit/monitrc"
+confdir_redhat="/etc/monit.d"
+conffile_redhat="/etc/monit.conf"
 
 function usage {
 	echo "Usage: $(basename $0) -e email [ -v ]" >&2
@@ -101,6 +106,31 @@ function error {
 	log "ERROR:  $1" "$log_out_err"
 }
 
+function f_guess_os {
+	os_family="unknown"
+	debug "trying to find out which OS you are using"
+	if grep -i centos /etc/issue
+	then
+		debug "seams you are using CentOS"
+		os_family="redhat"
+	elif grep -i redhat /etc/issue
+	then
+		debug "looks like you are running on RedHat"
+		os_family="redhat"
+	elif grep -i debian /etc/issue
+	then
+		debug "you are running a debian System"
+		os_family="debian"
+	elif grep -i ubuntu /etc/issue
+	then
+		debug "seems as if you are using Ubuntu"
+		os_family="debian"
+	else
+		error "Sorry, I could not guess your operating system, and therefore no automatic installation possible"
+		cleanup 3
+	fi
+}
+
 function f_get_system_info {
 	debug "To monitor accurate system load indication, we need to know how much cpus the system has got"
 	cpus=$(cat /proc/cpuinfo |grep processor|tail -n1 |cut -d":" -f2|tr -cd [:digit:])
@@ -109,7 +139,7 @@ function f_get_system_info {
 	load_50=$(echo "scale=1;$cpus/2" |bc)
 }
 
-function f_install {
+function f_install_redhat {
 	#install epel release if not installed already
 	if [ -e /etc/yum.repos.d/epel.repo ]
 	then
@@ -133,11 +163,28 @@ function f_install {
 	fi
 }
 
+function f_install_debian {
+	debug "testing if monit is installed"
+	if $(which monit &>/dev/null)
+	then
+		debug "monit seams to be installed, already"
+	else
+		debug "Installing monit as it doesn't seam to be installed"
+		apt-get -y install monit
+	fi
+}
+
 function f_configure {
+#confdir_debian="/etc/monit/conf.d/"
+#conffile_debian="/etc/monit/monitrc"
+#confdir_redhat="/etc/monit.d"
+#conffile_redhat="/etc/monit.conf"
+	local cf="conffile_$os_family"
+	local cd="confdir_$os_family"
 	debug "getting system info"
 	f_get_system_info
-	debug "writing  monit.conf"
-	cat >/etc/monit.conf <<EOF_MONIT_CONF
+	debug "writing  ${!cf}"
+	cat >${!cf}<<EOF_MONIT_CONF
 set daemon 120 #check every 2 minutes
 set logfile syslog facility log_daemon # log to syslog
 set mailserver localhost #by default send mails through locally running mail server
@@ -151,11 +198,11 @@ check system localhost
 	if loadavg (5min) > $cpus then alert #cpus may be replaced by $load_50, wthich is "used 50% of system resources"
 	if memory usage > 85% then alert #alert if more than 85% memory is used
 # include specific monitoring rules
-include /etc/monit.d/*
+include ${!cd}/*
 EOF_MONIT_CONF
-	mkdir -p /etc/monit.d/
+	mkdir -p ${!cd}
 	debug "creating searchblox monit conf"
-	cat >/etc/monit.d/searchblox.conf <<EOF_SB_MONIT_CONF
+	cat >${!cd}/searchblox.conf <<EOF_SB_MONIT_CONF
 check process searchblox_process with pidfile /var/run/wrapper.searchblox.pid
 	start program = "/etc/init.d/searchblox start" with timeout 60 seconds
 	stop program = "/etc/init.d/searchblox stop"
@@ -170,17 +217,33 @@ check host searchblox_service with address localhost
 check filesystem searchblox_folder with path /opt/searchblox
 	if space gt 75% then alert
 EOF_SB_MONIT_CONF
+	if [ -e /etc/monit.d/logging ]
+	then
+		debug "deleting logging file from /etc/monit.d/ which came with install"
+		rm -f /etc/monit.d/logging
+	fi
 	debug "finished"
+}
 
+function f_manage_service_debian {
+	debug "enabling autostart of monit"
+	update-rc.d monit defaults
+	debug "starting monit"
+	service monit start
+}
+
+function f_manage_service_redhat {
+	debug "enabling autostart of monit"
+	chkconfig monit on
+	debug "starting monit"
+	service monit start
 }
 
 parseOpts "$@"
 initialize
-f_install
+f_guess_os
+f_install_$os_family
 f_configure
-debug "enabling autostart of monit"
-chkconfig monit on
-debug "starting monit"
-service monit start
+f_manage_service_$os_family
 cleanup 0
 
